2d0187efbe79f1ab48a57f5591a7bcd2085cdd61
Joe Adams <jdfadams@msp.org>
2021-05-16 22:13:18 -0700

    Refactor

--- 2017-2018/usfli.py
+++ 2017-2018/usfli.py
@@ -1,8 +1,15 @@
+from pprint import pprint
+
 import numpy as np
 import pandas as pd
 
 
-def main():
+non_alcoholic = lambda df: (((df.sex == 'female') & (df.drinks <= 1)) | ((df.sex == 'male') & (df.drinks <= 2)))
+
+no_liver_disease = lambda df: ~(df.hep_b | df.hep_c | df.liver_cancer | df.other_liver_condition)
+
+
+def usfli(threshold=30):
     alcohol = pd.read_sas('ALQ_J.XPT')
     hepatitis = pd.read_sas('HEQ_J.XPT')
     medical_conditions = pd.read_sas('MCQ_J.XPT')
@@ -23,7 +30,8 @@ def main():
         'glucose': len(glucose),
         'insulin': len(insulin),
     }
-    print('Numbers of rows:', lengths)
+    print('Numbers of rows:')
+    pprint(lengths)
 
     df = pd.DataFrame()
     df['drinks'] = alcohol['ALQ130']
@@ -62,19 +70,86 @@ def main():
         - 14.7812
     )
     df['usfli'] = np.exp(df['z']) / (1 + np.exp(df['z'])) * 100
-    nafld_fli = df[
-        (df.usfli >= 60) &
-        (((df.sex == 'female') & (df.drinks <= 1)) | ((df.sex == 'male') & (df.drinks <= 2))) &
-        (~(df.hep_b | df.hep_c | df.liver_cancer | df.other_liver_condition))
+    nafld_usfli = df[
+        (df.usfli >= threshold) & non_alcoholic(df) & no_liver_disease(df)
     ]
     nafld_diagnosed = df[
-        df.fatty_liver &
-        (((df.sex == 'female') & (df.drinks <= 1)) | ((df.sex == 'male') & (df.drinks <= 2))) &
-        (~(df.hep_b | df.hep_c | df.liver_cancer | df.other_liver_condition))
+        df.fatty_liver & non_alcoholic(df) & no_liver_disease(df)
     ]
     print('Number of rows with USFLI:', len(df))
-    print('NAFLD (according to USFLI):', len(nafld_fli))
+    print(f'NAFLD (USFLI >= {threshold}):', len(nafld_usfli))
     print('NAFLD (diagnosed with FLD):', len(nafld_diagnosed))
+
+
+def fli(threshold=60):
+    alcohol = pd.read_sas('ALQ_J.XPT')
+    demographics = pd.read_sas('DEMO_J.XPT')
+    hepatitis = pd.read_sas('HEQ_J.XPT')
+    medical_conditions = pd.read_sas('MCQ_J.XPT')
+
+    biochemistry = pd.read_sas('BIOPRO_J.XPT')
+    body = pd.read_sas('BMX_J.XPT')
+    triglycerides = pd.read_sas('TRIGLY_J.XPT')
+
+    lengths = {
+        'alcohol': len(alcohol),
+        'demographics': len(demographics),
+        'hepatitis': len(hepatitis),
+        'medical_conditions': len(medical_conditions),
+
+        'biochemistry': len(biochemistry),
+        'body': len(body),
+        'triglycerides': len(triglycerides),
+    }
+    print('Numbers of rows:')
+    pprint(lengths)
+
+    df = pd.DataFrame()
+    df['drinks'] = alcohol['ALQ130']
+    df['hep_b'] = hepatitis['HEQ010'].apply(lambda x: x == 1)
+    df['hep_c'] = hepatitis['HEQ030'].apply(lambda x: x == 1)
+    df['liver_cancer'] = (
+        medical_conditions['MCQ230A'].apply(lambda x: x == 22) |
+        medical_conditions['MCQ230B'].apply(lambda x: x == 22) |
+        medical_conditions['MCQ230C'].apply(lambda x: x == 22)
+    )
+    df['fatty_liver'] = medical_conditions['MCQ510A'].apply(lambda x: x == 1)
+    df['other_liver_condition'] = (
+        medical_conditions['MCQ510B'].apply(lambda x: x == 2) |
+        medical_conditions['MCQ510C'].apply(lambda x: x == 3) |
+        medical_conditions['MCQ510D'].apply(lambda x: x == 4) |
+        medical_conditions['MCQ510E'].apply(lambda x: x == 5) |
+        medical_conditions['MCQ510F'].apply(lambda x: x == 6)
+    )
+    df['sex'] = demographics['RIAGENDR'].apply(lambda x: 'male' if x == 1 else 'female')
+    df['ggt'] = biochemistry['LBXSGTSI']
+    df['triglycerides'] = triglycerides['LBXTR']
+    df['bmi'] = body['BMXBMI']
+    df['waist_circumference'] = body['BMXWAIST']
+    df = df.dropna()
+    df['z'] = (
+        0.953 * np.log(df['triglycerides'])
+        + 0.139 * df['bmi']
+        + 0.718 * np.log(df['ggt'])
+        + 0.053 * df['waist_circumference']
+        - 15.745
+    )
+    df['fli'] = np.exp(df['z']) / (1 + np.exp(df['z'])) * 100
+    nafld_fli = df[
+        (df.fli >= threshold) & non_alcoholic(df) & no_liver_disease(df)
+    ]
+    nafld_diagnosed = df[
+        df.fatty_liver & non_alcoholic(df) & no_liver_disease(df)
+    ]
+    print('Number of rows with FLI:', len(df))
+    print(f'NAFLD (FLI >= {threshold}):', len(nafld_fli))
+    print('NAFLD (diagnosed with FLD):', len(nafld_diagnosed))
+
+
+def main():
+    usfli()
+    print('-' * 40)
+    fli()
 
 
 if __name__ == '__main__':
